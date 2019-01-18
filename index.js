80ceb29c98572674c02c5b476e82fbec1143a3f3
FreitagDavid <freitagdavid@outlook.com>
2019-01-18 11:26:17 -0500

    index:  /projects POST endpoint complete

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const port = 5000;
+const server = express();
+server.use(express.json());
+const db = knex(knexConfig.development);
+
+server.get('/', ({ res }) => {
+    res.status(200).json({ success: 'All is well' });
+});
+
+server.post('/projects', (req, res) => {
+    const data = req.body;
+    db('projects')
+        .insert(data)
+        .then(result => {
+            res.status(200).json({ result });
+        })
+        .catch(err => {
+            if (err.errno === 19) {
+                res.status(400).json({ errMessage: 'Name is required' });
+            } else {
+                res.status(500).json(err);
+            }
+        });
+});
+
+server.listen(port, () => console.log('Server is listening on port', port));
